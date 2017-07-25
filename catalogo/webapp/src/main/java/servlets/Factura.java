68a5be37ed3d819e983ade294f5c15cc651741f3
McCoy9000 <mcmilbao@gmail.com>
2017-07-25 20:16:57 +0200

    Factura servlet

---
+++ catalogo/webapp/src/main/java/servlets/Factura.java
@@ -0,0 +1,37 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/factura")
+public class Factura extends HttpServlet {
+
+	private static final long serialVersionUID = -374351075129665738L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doPost(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String op = request.getParameter("op");
+
+		if (op == null) {
+			request.getRequestDispatcher("/WEB-INF/vistas/agradecimiento.jsp").forward(request, response);
+		} else {
+			switch (op) {
+			case "factura":
+				request.getRequestDispatcher("/WEB-INF/vistas/factura.jsp").forward(request, response);
+				break;
+			}
+		}
+
+	}
+}
