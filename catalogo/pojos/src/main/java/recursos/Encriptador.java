9cdcade65142392f2deac7ad8102d89487849eda
McCoy9000 <mcmilbao@gmail.com>
2017-08-04 21:23:38 +0200

    Commit de recuperación de master frente a AWS con cambios en persistences xml y Encriptador en pojos/recursos

---
+++ catalogo/pojos/src/main/java/recursos/Encriptador.java
@@ -0,0 +1,44 @@
+package recursos;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Encriptador {
+
+	String Key = "todostusmuertos";
+	byte[] KeyData = Key.getBytes();
+
+	public SecretKeySpec KS = new SecretKeySpec(KeyData, "Blowfish");
+
+	public Cipher cipher = Cipher.getInstance("Blowfish");
+
+	public Encriptador() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+
+		cipher.init(Cipher.ENCRYPT_MODE, KS);
+
+	}
+
+	public String encriptar(String password) {
+		
+		
+		byte[] encPassword = null;
+		
+		if (password != null) {
+			try {
+				encPassword = this.cipher.doFinal(password.getBytes());
+			} catch (IllegalBlockSizeException | BadPaddingException e1) {
+				e1.printStackTrace();
+			}
+			password = Base64.getMimeEncoder().encodeToString(encPassword);
+		}
+		
+		return password;
+	}
+}
