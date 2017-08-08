4d84e7080cf002b7776dd284a0dff48ab2904bda
McCoy9000 <mcmilbao@gmail.com>
2017-08-08 18:35:24 +0200

    Creados métodos en PersonaFactory

--- catalogo/pojos/src/main/java/factories/PersonaFactory.java
+++ catalogo/pojos/src/main/java/factories/PersonaFactory.java
@@ -1,9 +1,34 @@
 package factories;
 
+import java.time.LocalDate;
+
+import pojos.Direccion;
+import pojos.Empresa;
+import pojos.IPersona;
+import pojos.Imagen;
+import pojos.Persona;
+import pojos.Rol;
+import pojos.Usuario;
+
 public class PersonaFactory {
 
 	public PersonaFactory() {
-		// TODO Auto-generated constructor stub
 	}
 
+	public IPersona getPersona() {
+		return new Persona();
+	}
+	
+	public IPersona getUsuario() {
+		return new Usuario();
+	}
+	
+	public IPersona getUsuario(String nombre, String apellidos, String email, String username, String password, Rol rol) {
+		return new Usuario(nombre, apellidos, email, username, password, rol);
+	}
+	
+	public IPersona getUsuario(String nombre, String apellidos, LocalDate fechaNacimiento, String documento, String telefono, String email, Direccion direccion, Empresa empresa, Imagen imagen, String username,
+			String password) {
+		return new Usuario(nombre, apellidos, fechaNacimiento, documento, telefono, email, direccion, empresa, imagen, username, password);
+	}
 }
